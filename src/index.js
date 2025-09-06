cbfde36722d5f168dcb5c42fcc2ab144ded6cdf6
Robo <154452780+sumitkshatrya@users.noreply.github.com>
2025-09-06 18:11:37 +0530

    0

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import path from "path";
+
 
 import { connectDB } from "./lib/db.js";
 import authRoute from "./routes/auth.route.js";
@@ -11,8 +11,8 @@ import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();   // 
+const PORT = process.env.PORT || 5001;
+
 
 // Middlewares
 app.use(express.json({ limit: "100mb" }));
@@ -30,14 +30,6 @@ app.use(
 app.use("/api/auth", authRoute);
 app.use("/api/messages", messageRoute);
 
-//  Serve frontend in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/dist")));
-
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/client/dist/index.html"));
-  });
-}
 
 // Start server + DB
 server.listen(PORT, () => {
